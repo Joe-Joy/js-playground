d5745adeb851499facde24864e80b81bc2166dfa
joejoy <joejoy456999@gmail.com>
2021-02-24 17:15:20 +0530

    completed

--- first.js
+++ first.js
@@ -1,33 +1,41 @@
-var a = 10,
-  b = "ashli",
-  c = 10.2;
-console.log("%s,%s,%s", a, b, c);
+// var a = 10,
+//   b = "ashli",
+//   c = 10.2;
+// console.log("%s,%s,%s", a, b, c);
 
-// object
+// // object
 
-var human = {
-  name: "jino",
-  age: 30,
-  eye: 2,
-  ear: 2,
-};
+// var human = {
+//   name: "jino",
+//   age: 30,
+//   eye: 2,
+//   ear: 2,
+// };
 
-console.log(human.name, human.age);
+// console.log(human.name, human.age);
 
-var animals = {
-  Email: "",
-  Groups: {},
-  Id: 33,
-  IsHiddenInUI: false,
-  IsSiteAdmin: false,
-  LoginName: "i:0#.w|virtualdomain\\user2",
-  PrincipalType: 1,
-  Title: "user2",
-  whereis: {},
-};
+// var animals = {
+//   Email: "",
+//   Groups: {},
+//   Id: 33,
+//   IsHiddenInUI: false,
+//   IsSiteAdmin: false,
+//   LoginName: "i:0#.w|virtualdomain\\user2",
+//   PrincipalType: 1,
+//   Title: "user2",
+//   whereis: {},
+// };
 
-console.log(animals.Email);
+// console.log(animals.Email);
 
-var element = document.createElement("p");
-element.textContent = "Hello, World";
-document.body.appendChild(element);
+// var element = document.createElement("p");
+// element.textContent = "Hello, World";
+// document.body.appendChild(element);
+
+
+
+var x=0;
+do{
+  console.log(x);
+  x=x+3;
+}while(x<10);
--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function change() {
-    document.getElementById('demo').innerHTML = 'Hello JavaScript!'
-    var element = document.createElement('demo');
-    element.textContent = "Hello, World";
-    document.body.appendChild(element);
+  document.getElementById("demo").innerHTML = "Hello JavaScript!";
+  var element = document.createElement("demo");
+  element.textContent = "Hello, World";
+  document.body.appendChild(element);
 }
 
+function changes() {
+  document.getElementById("demo").innerHTML = "Hellosssss!";
+}
 
+function currentTime() {
+  document.getElementById("demo").innerHTML = Date();
+}
+
+document.getElementById("myBtn").addEventListener("click", function () {
+  /*(within a single function onclick method)*/ this.innerHTML = "hi";
+});
