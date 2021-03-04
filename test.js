46caa568d27d096ff161c3c6100cfbdd54e194dd
joejoy <joejoy456999@gmail.com>
2021-03-04 12:01:23 +0530

    completed

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log('test server')
+
+function date() {
+  var d = new Date();
+  return d.toLocaleDateString();
+}
+
+function Time() {
+  var d = new Date();
+  return d.toLocaleTimeString();
+}
+
+console.log(date(), Time());
+console.log(date());
+console.log(Time());
